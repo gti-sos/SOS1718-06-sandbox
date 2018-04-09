4e8cafe341ae434427bb169c21c1fa23886f765f
sos171806dgc <dionigc96@gmail.com>
2018-04-09 20:01:02 +0000

    l08

--- index.js
+++ index.js
@@ -1,202 +1,96 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var DataStore = require("nedb");
+var path = require("path");
+
 var port = (process.env.PORT || 1607);
-
 var BASE_API_PATH = "/api/v1";
-var BASE_API_PATH_BUDGETS = "/api/v1/budgets-generals";
-var BASE_API_PATH_HELP = "/api/v1/help";
-
-var dbBudgets= __dirname + "budgets-generals.db";
 
 var app = express();
 
 app.use(bodyParser.json());
-app.get(BASE_API_PATH_HELP,(req,res)=>{
-    res.redirect("https://documenter.getpostman.com/collection/view/3895452-62614861-fe34-4ef4-83a2-a39409f16891#443a5885-f455-4f36-b0e1-3fc2a43728d1");
-});
 
+app.use("/",express.static(path.join(__dirname,"public")));
 
-var budgets = [
+var contacts = [
         { 
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "parlamento-de-andalucia",
-            "total-budget" : "45.479.748",
-            "percentage-over-total" : 0.137,
+            "name" : "pablo",
+            "phone" : 12345   
         },
-         { 
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "deuda-publica",
-            "total-budget" : "4.162.050.097",
-            "percentage-over-total" : 12.521,
-        },
-         { 
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "camara-de-cuentas",
-            "total-budget" : "10.408.549",
-            "percentage-over-total" : 31,
-        },
-         { 
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "consejeria-de-educacion",
-            "total-budget" : "6.100.323.433",
-            "percentage-over-total" : 18.353,
-        },
-         { 
-            "community" : "andalucia",
-            "year" : 2017,
-            "section" : "consejeria-de-salud",
-            "total-budget" : "725.885.406",
-            "percentage-over-total" : 2.184,
-        },
-       
-       
+        { 
+            "name" : "pepe",
+            "phone" : 6789   
+        }
     ];
-    
-//////base de Datos de budgets////////
-var db = new DataStore({  //inicializar base de datos
-    filename: dbBudgets,
-    autoload: true
-}) ; 
 
-//loadInitial de Budgets
-app.get(BASE_API_PATH_BUDGETS + "/loadInitialData", (req, res) => {
-    db.find({}, (err, budget) => {
-        if (err) {
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-        }
-        else if (budget.length == 0) {
-            db.insert(budgets);
-            budgets.push(budgets);
-            console.log("DB initialized with " + budgets.length + " budgets");
-            res.sendStatus(200)
-        }
-        else{
-            console.log("DB initialized with " + budgets.length + " budgets.")
-            res.sendStatus(200);
-        }
-    });
-});
-    
-db.find({},(err,budgetsGenerals)=>{
-    if(err){
-        console.error("Error accesing DB");
-        process.exit(1);
-    }
-    if(budgetsGenerals.length == 0){
-        console.log("Empty DB");
-        db.insert(budgets);
-    }else{
-        console.log("DB initialized with" + budgetsGenerals.length + " budgets");
-    }
-});    
-//////-----budgets-generals------///////
-
-app.get(BASE_API_PATH_BUDGETS,(req,res)=>{
-    console.log(Date() + " - GET /budgets-generals");
-    
-    db.find({},(err,budgetsGenerals)=>{
-    if(err){
-        console.error("Error accesing DB");
-        res.sendStatus(500);
-        return;
-    }
-       res.send(budgets);
- 
+app.get(BASE_API_PATH+"/contacts",(req,res)=>{
+    console.log(Date() + " - GET /contacts");
+    res.send(contacts);
 });
 
-app.post(BASE_API_PATH_BUDGETS,(req,res)=>{
-    console.log(Date() + " - POST /budgets-generals");
-    db.insert(req.body, (err, budget) => {
-        if (err) {
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-            return;
-        }
-        
-        res.sendStatus(201);
-    });
+app.post(BASE_API_PATH+"/contacts",(req,res)=>{
+    console.log(Date() + " - POST /contacts");
+    var contact = req.body;
+    contacts.push(contact);
+    res.sendStatus(201);
 });
 
-app.put(BASE_API_PATH_BUDGETS,(req,res)=>{ // debe dar un error de método no permitido
-    console.log(Date() + " - PUT /budgets-generals"); 
+app.put(BASE_API_PATH+"/contacts",(req,res)=>{
+    console.log(Date() + " - PUT /contacts");
     res.sendStatus(405);
 });
 
-app.delete(BASE_API_PATH_BUDGETS,(req,res)=>{
-    console.log(Date() + " - DELETE /budgets-generals/");
-    db.remove({});
+app.delete(BASE_API_PATH+"/contacts",(req,res)=>{
+    console.log(Date() + " - DELETE /contacts");
+    contacts = [];
     res.sendStatus(200);
 });
 
 
-app.get(BASE_API_PATH_BUDGETS+"/:section",(req,res)=>{
-    var section = req.params.section;
-    console.log(Date() + " - GET /budgets-generals/"+section);
+app.get(BASE_API_PATH+"/contacts/:name",(req,res)=>{
+    var name = req.params.name;
+    console.log(Date() + " - GET /contacts/"+name);
     
-    res.send(budgets.filter((b)=>{
-        return (b.section == section);
+    res.send(contacts.filter((c)=>{
+        return (c.name == name);
     })[0]);
 });
 
-
-app.delete(BASE_API_PATH_BUDGETS+"/:section",(req,res)=>{
-    var section = req.params.section;
-    console.log(Date() + " - DELETE /budgets-generals/"+section);
+app.delete(BASE_API_PATH+"/contacts/:name",(req,res)=>{
+    var name = req.params.name;
+    console.log(Date() + " - DELETE /contacts/"+name);
     
-    budgets = budgets.filter((b)=>{
-        return (b.section != section);
+    contacts = contacts.filter((c)=>{
+        return (c.name != name);
     });
     
     res.sendStatus(200);
 });
 
-app.delete(BASE_API_PATH_BUDGETS+"/:section",(req,res)=>{
-     var section1 = req.params.section;
-    console.log(Date() + " - DELETE /budgets-generals/"+section);
-     db.remove({section:section1}, {multi: true}, (err, budget) => {
-        if (err) {
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-            return;
-        }
-        
-        res.sendStatus(200);
-    });
-    
-});
-
-app.post(BASE_API_PATH_BUDGETS+"/:section",(req,res)=>{ //debe dar error de método no permitido
-    var section = req.params.section;
-    console.log(Date() + " - POST /budgets-generals/"+section);
+app.post(BASE_API_PATH+"/contacts/:name",(req,res)=>{
+    var name = req.params.name;
+    console.log(Date() + " - POST /contacts/"+name);
     res.sendStatus(405);
 });
 
-app.put(BASE_API_PATH_BUDGETS+"/:section",(req,res)=>{
-    var section = req.params.section;
-    var b = req.body;
+app.put(BASE_API_PATH+"/contacts/:name",(req,res)=>{
+    var name = req.params.name;
+    var contact = req.body;
     
-    console.log(Date() + " - PUT /budgets-generals/"+section);
+    console.log(Date() + " - PUT /contacts/"+name);
     
-   if (section != b.section) {
+    if(name != contact.name){
         res.sendStatus(409);
-        console.warn(Date() + " - Hacking attempt!");
+        console.warn(Date()+" - Hacking attempt!");
         return;
     }
     
-    db.update({"section": b.section},b,(err,numUpdated)=>{
-        if (err) {
-            console.error("Error accesing DB");
-            res.sendStatus(500);
-            return;
-        }
-        console.log("Updated: " + numUpdated);
+    contacts = contacts.map((c)=>{
+        if(c.name == contact.name)
+            return contact;
+        else
+            return c;
     });
+    
     res.sendStatus(200);
 });
 
